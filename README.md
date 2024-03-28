32734eff082f34ea4587511a6cbd5a9054396d79
tabby8612 <tabishsajwani@hotmail.com>
2024-03-28 11:31:59 +0400

    Application Added

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # circular-progress-bar
+
+This is the tutorial fo progress bar.
+
+Here is the screenshot:
+
+![screenshot](screenshot.jpg)
---
+++ script.js
@@ -0,0 +1,12 @@
+const number = document.getElementById("number");
+let counter = 0;
+
+setInterval(() => {
+
+    if (counter == 65) {
+        clearInterval();
+    } else {
+        counter++;
+        number.innerHTML = counter + "%";
+    }
+}, 30);
